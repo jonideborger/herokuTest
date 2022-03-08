680a9c6598c1bdfc1cfb89decf7987161a3fa143
Jan Everaert <jan@crashlab.be>
2022-03-08 11:46:02 +0100

    adding simple db

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
 const express = require('express')
-const app = express()
 const PORT = process.env.PORT || 3000;
+const { JsonDB } = require('node-json-db');
+const { Config } = require('node-json-db/dist/lib/JsonDBConfig');
+const bodyParser = require('body-parser');
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var db = new JsonDB(new Config("chat", true, false, '/'));
+db.push("/messages", [{message:"test", author: "test"}]);
+db.save();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  var data = db.getData("/messages");
+  res.json(data)
+})
+
+app.post('/message', (req, res) => {
+  console.log(req.body)
+  const inserting = {...req.body, id: makeid(6)};
+  const data = db.getData("/messages");
+  data.push(inserting);
+  db.push("/messages", data);
+  db.save();
+  res.json(inserting)
 })
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
+
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+charactersLength));
+ }
+ return result;
+}
