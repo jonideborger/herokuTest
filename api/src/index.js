f035a2492afd9886868d95e4562e3077b80a3fd3
Jan Everaert <jan@crashlab.be>
2022-03-09 09:22:23 +0100

    update all endpoints to db

--- api/src/index.js
+++ api/src/index.js
@@ -1,16 +1,14 @@
 const express = require('express')
 const PORT = process.env.PORT || 3000;
-const { JsonDB } = require('node-json-db');
-const { Config } = require('node-json-db/dist/lib/JsonDBConfig');
 const bodyParser = require('body-parser');
 const path = require('path')
 const requestIp = require("request-ip");
-const { default: knex } = require('knex');
 
 const pg = require('knex')({
   client: 'pg',
   connection: process.env.DATABASE_URL+"?ssl=true",
   searchPath: ['knex', 'public'],
+  ssl: { rejectUnauthorized: false }
 });
 
 pg.schema.hasTable('messages').then(function(exists) {
@@ -34,25 +32,11 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var db = new JsonDB(new Config("chat", true, false, '/'));
-db.push("/messages", []);
-db.save();
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 
 app.get('/messages', (req, res) => {
-  var data = db.getData("/messages");
-  res.json(data.map((e) => {
-    return {
-      author: e.author,
-      message: e.message,
-      id: e.id
-    }
-  }))
-})
-
-app.get('/db/messages', (req, res) => {
   pg.select("*").table("messages").then((data) => {
     res.json(data.map((e) => {
       return {
@@ -65,21 +49,12 @@ app.get('/db/messages', (req, res) => {
   })
 })
 app.get('/debug', (req, res) => {
-  var data = db.getData("/messages");
-  res.json(data)
+  pg.select("*").table("messages").then((data) => {
+    res.json(data);
+  });
 })
 
 app.post('/message', (req, res) => {
-  const clientIp = requestIp.getClientIp(req);
-  const hostname = req.hostname;
-  const inserting = {...req.body, id: makeid(6)};
-  const data = db.getData("/messages");
-  data.push({ ...inserting, ip: clientIp, hostname: hostname });
-  db.push("/messages", data);
-  db.save();
-  res.json(inserting)
-})
-app.post('/db/message', (req, res) => {
   const clientIp = requestIp.getClientIp(req);
   const hostname = req.hostname;
   const inserting = {...req.body, handle: makeid(6)};
@@ -89,45 +64,23 @@ app.post('/db/message', (req, res) => {
 })
 
 app.delete('/message/:id', (req, res) => {
-  const data = db.getData("/messages");
-  const filtered = data.filter((e) => e.id !== req.params.id)
-  db.push("/messages", filtered);
-  db.save();
-  res.send("deleted "+req.params.id)
+  pg.delete().table("messages").where({handle: req.params.id}).then((data) => {
+    res.send("deleted "+req.params.id)
+  })
 })
+
+
 app.patch('/message/:id', (req, res) => {
-  const data = db.getData("/messages");
-  const mapped = data.map((e) => {
-    if(e.id == req.params.id) {
-      return {
-        ...e,
-        ...req.body
-      }
-    }
-    return e
+  pg.update({...req.body}).table("messages").where({handle: req.params.id}).then((data) => {
+    res.send("updated "+req.params.id)
   })
-
-  db.push("/messages", mapped);
-  db.save();
-  res.send("updated "+req.params.id)
 })
-
 app.put('/message/:id', (req, res) => {
-  const data = db.getData("/messages");
-  const mapped = data.map((e) => {
-    if(e.id == req.params.id) {
-      return {
-        ...e,
-        ...req.body
-      }
-    }
-    return e
+  pg.update({...req.body}).table("messages").where({handle: req.params.id}).then((data) => {
+    res.send("updated "+req.params.id)
   })
-
-  db.push("/messages", mapped);
-  db.save();
-  res.send("updated "+req.params.id)
 })
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
