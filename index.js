ee53ad7c188025ef83e6407d229bdda462d55bcd
Joni De Borger <joni.de.borger@ehb.be>
2023-03-14 13:27:29 +0100

    back to 3000

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const __filename = fileURLToPath(
     import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 const pg = knex({
     client: KnexMysql,
     connection: {
